8c2fa74aaea1ff7bbba7440e3d76e7798c404abf
jbsselim <selim.jean-baptiste-simonne@epitech.eu>
2022-04-09 03:31:49 +0200

    minor change

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,5 +1,5 @@
-import {useSession} from "next-auth/react"
-import { signIn,useEffect } from "react";
+import {signIn,useSession} from "next-auth/react"
+import { useEffect } from "react";
 import SpotifyWebApi from "spotify-web-api-node";
 
 const spotifyApi  = new SpotifyWebApi({
